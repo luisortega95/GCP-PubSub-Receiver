4f5facbf980995cb3c3fbb80a107174a21a5495c
Luis Ortega Mantilla <luis.ortega@sofka.local>
2022-10-05 17:39:55 -0500

    Initial commit

---
+++ src/test/java/com/sofka/gcppubsubreceiver/GcpPubSubReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.gcppubsubreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPubSubReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
